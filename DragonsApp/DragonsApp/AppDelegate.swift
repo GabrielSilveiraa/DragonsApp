9af49897a76c4c3475a8dc5d7a1b58d4dfe4e834
Gabriel Silveira <gabi.msilveira@gmail.com>
2019-06-11 21:48:55 -0300

    Created xcode project

---
+++ DragonsApp/DragonsApp/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  DragonsApp
+//
+//  Created by Gabriel Silveira on 11/06/19.
+//  Copyright © 2019 Gabriel Silveira. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ DragonsApp/DragonsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DragonsApp
+//
+//  Created by Gabriel Silveira on 11/06/19.
+//  Copyright © 2019 Gabriel Silveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
