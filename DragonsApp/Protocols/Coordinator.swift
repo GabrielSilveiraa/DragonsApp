2f41bd68bc15ee7973a6a0fc6f263388a2a57c86
Gabriel Silveira <gabi.msilveira@gmail.com>
2019-06-12 01:39:56 -0300

    Created Coordinator protocol

---
+++ DragonsApp/Protocols/Coordinator.swift
@@ -0,0 +1,9 @@
+//
+//  Coordinator.swift
+//  DragonsApp
+//
+//  Created by Gabriel Silveira on 12/06/19.
+//  Copyright © 2019 Gabriel Silveira. All rights reserved.
+//
+
+import Foundation
---
+++ DragonsApp/Scenes/DragonsList/DragonsListCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  DragonsListCoordinator.swift
+//  DragonsApp
+//
+//  Created by Gabriel Silveira on 12/06/19.
+//  Copyright © 2019 Gabriel Silveira. All rights reserved.
+//
+
+import Foundation
